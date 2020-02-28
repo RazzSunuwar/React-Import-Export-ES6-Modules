549b3360da1c34dcc836aad08ead56566470d63f
RazzSunuwar <razzkoinch5@gmail.com>
2020-02-28 14:18:44 +0545

    Import and Export (ES6 Modules)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+// named and default import/export
+// only one default export module
+// think one module as file
+
 import React from 'react';
+import {name, age, person} from './data';
 
 const App = () => (
   <section>
   <p>This is is my content.</p>
+<p>{name}</p>
+<p>{age}</p>
   </section>
 );
 
---
+++ src/data.js
@@ -0,0 +1,9 @@
+export const name = "Razor";
+export const age = 26;
+
+export const person = {
+    name:"Brad",
+    age: 40
+};
+
+const secretValue = "secret";
