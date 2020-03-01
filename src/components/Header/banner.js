e933a1bfed10b4b9b5128774f39e66544eea3869
RazzSunuwar <razzkoinch5@gmail.com>
2020-03-01 13:02:11 +0545

    Hello from Banner update

--- src/components/Header/banner.js
+++ src/components/Header/banner.js
@@ -1,8 +1,22 @@
 import React from 'react';
-export default function Banner(){
+import { name, age, person } from '../../data';
+// rfc => functional based component
+// rcc => class based component
+
+// export default function Banner(){
+//     return (
+//         <div>
+//             <h1>Hello From Banner {name} </h1>
+//         </div>
+//     );
+// };
+
+function Banner (){
     return (
         <div>
-            <h1>Hello From Banner </h1>
+            <h1>Hello From Banner {name} </h1>
         </div>
     );
 };
+
+export default Banner;
